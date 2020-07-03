5eab088d71f7cc26bd3afad568502cac6982af21
TnTBass <tyler.jurgens@gmail.com>
2020-07-02 19:48:16 -0600

    Update entrypoint to be verium specific

--- bin/docker_entrypoint.sh
+++ bin/docker_entrypoint.sh
@@ -2,14 +2,14 @@
 
 set -euo pipefail
 
-BITCOIN_DIR=/root/.bitcoin
-BITCOIN_CONF=${BITCOIN_DIR}/bitcoin.conf
+VERIUM_DIR=/root/.verium
+VERIUM_CONF=${VERIUM_DIR}/verium.conf
 
 # If config doesn't exist, initialize with sane defaults for running a
 # non-mining node.
 
-if [ ! -e "${BITCOIN_CONF}" ]; then
-  tee -a >${BITCOIN_CONF} <<EOF
+if [ ! -e "${VERIUM_CONF}" ]; then
+  tee -a >${VERIUM_CONF} <<EOF
 
 # For documentation on the config file, see
 #
@@ -18,50 +18,44 @@ if [ ! -e "${BITCOIN_CONF}" ]; then
 # the wiki:
 #   https://en.bitcoin.it/wiki/Running_Bitcoin
 
-# server=1 tells Bitcoin-Qt and bitcoind to accept JSON-RPC commands
+# server=1 tells verium-Qt and veriumd to accept JSON-RPC commands
 server=1
 
 # You must set rpcuser and rpcpassword to secure the JSON-RPC api
-rpcuser=${BTC_RPCUSER:-btc}
-rpcpassword=${BTC_RPCPASSWORD:-changemeplz}
+rpcuser=${VRM_RPCUSER:-btc}
+rpcpassword=${VRM_RPCPASSWORD:-changemeplz}
 
-# How many seconds bitcoin will wait for a complete RPC HTTP request.
+# How many seconds verium will wait for a complete RPC HTTP request.
 # after the HTTP connection is established.
-rpcclienttimeout=${BTC_RPCCLIENTTIMEOUT:-30}
+rpcclienttimeout=${VRM_RPCCLIENTTIMEOUT:-30}
 
-rpcallowip=${BTC_RPCALLOWIP:-::/0}
+rpcallowip=${VRM_RPCALLOWIP:-::/0}
 
 # Listen for RPC connections on this TCP port:
-rpcport=${BTC_RPCPORT:-8332}
+rpcport=${VRM_RPCPORT:-8332}
 
-# Print to console (stdout) so that "docker logs bitcoind" prints useful
+# Print to console (stdout) so that "docker logs veriumd" prints useful
 # information.
-printtoconsole=${BTC_PRINTTOCONSOLE:-1}
+printtoconsole=${VRM_PRINTTOCONSOLE:-1}
 
 # We probably don't want a wallet.
-disablewallet=${BTC_DISABLEWALLET:-1}
+disablewallet=${VRM_DISABLEWALLET:-1}
 
 # Enable an on-disk txn index. Allows use of getrawtransaction for txns not in
 # mempool.
-txindex=${BTC_TXINDEX:-0}
+txindex=${VRM_TXINDEX:-0}
 
-# Run on the test network instead of the real bitcoin network.
-testnet=${BTC_TESTNET:-0}
+# Run on the test network instead of the real verium network.
+testnet=${VRM_TESTNET:-0}
 
 # Set database cache size in MiB
-dbcache=${BTC_DBCACHE:-512}
-
-# ZeroMQ notification options:
-zmqpubrawblock=${BTC_ZMQPUBRAWBLOCK:-tcp://0.0.0.0:28333}
-zmqpubrawtx=${BTC_ZMQPUBRAWTX:-tcp://0.0.0.0:28333}
-zmqpubhashtx=${BTC_ZMQPUBHASHTX:-tcp://0.0.0.0:28333}
-zmqpubhashblock=${BTC_ZMQPUBHASHBLOCK:-tcp://0.0.0.0:28333}
+dbcache=${VRM_DBCACHE:-512}
 
 EOF
 fi
 
 if [ $# -eq 0 ]; then
-  exec bitcoind -datadir=${BITCOIN_DIR} -conf=${BITCOIN_CONF}
+  exec veriumd -datadir=${VERIUM_DIR} -conf=${VERIUM_CONF}
 else
   exec "$@"
 fi
